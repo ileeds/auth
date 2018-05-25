dcb08056973cd1594fada0d6d4802c41fc33c3e7
ileeds <ileeds@brandeis.edu>
2018-05-25 14:18:34 -0400

    react native app simple auth

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, CardSection, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCB2RyRwLpEk8BPEa_aSpr6v92qL1KLY6M',
+      authDomain: 'authentication-d9dd5.firebaseapp.com',
+      databaseURL: 'https://authentication-d9dd5.firebaseio.com',
+      projectId: 'authentication-d9dd5',
+      storageBucket: 'authentication-d9dd5.appspot.com',
+      messagingSenderId: '616452217021'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <CardSection>
+            <Spinner size='large' />
+          </CardSection>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
